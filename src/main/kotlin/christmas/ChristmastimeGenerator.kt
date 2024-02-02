ec2fe83c13ba4fbd85df52ab6a275c35381334e8
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-02-02 10:49:13 +0000

    ♻ associate level with the number of spaces

--- src/main/kotlin/christmas/ChristmastimeGenerator.kt
+++ src/main/kotlin/christmas/ChristmastimeGenerator.kt
@@ -6,29 +6,29 @@ class ChristmastimeGenerator(val printable: Printable) {
         giveOnlyTheTrunkWhenLevelIsZero(level)
         if (level == 2) {
             val tree = listOf(
-                " ".repeat(1) + "x".repeat(1) + "\n",
+                " ".repeat(level - 1) + "x".repeat(1) + "\n",
                 " ".repeat(0) + "x".repeat(3) + "\n",
-                " ".repeat(1) + "x".repeat(0) + "|"
+                " ".repeat(level - 1) + "x".repeat(0) + "|"
             )
             printable.print(tree.joinToString(separator = "") { it })
         }
         if (level == 3) {
             val tree = listOf(
-                " ".repeat(2) + "x".repeat(1) + "\n",
-                " ".repeat(1) + "x".repeat(3) + "\n",
+                " ".repeat(level - 1) + "x".repeat(1) + "\n",
+                " ".repeat(level - 2) + "x".repeat(3) + "\n",
                 " ".repeat(0) + "x".repeat(5) + "\n",
-                " ".repeat(2) + "x".repeat(0) + "|"
+                " ".repeat(level - 1) + "x".repeat(0) + "|"
             )
             printable.print(tree.joinToString(separator = "") { it })
         }
 
         if (level == 4) {
             val tree = listOf(
-                " ".repeat(3) + "x".repeat(1) + "\n",
-                " ".repeat(2) + "x".repeat(3) + "\n",
-                " ".repeat(1) + "x".repeat(5) + "\n",
+                " ".repeat(level - 1) + "x".repeat(1) + "\n",
+                " ".repeat(level - 2) + "x".repeat(3) + "\n",
+                " ".repeat(level - 3) + "x".repeat(5) + "\n",
                 " ".repeat(0) + "x".repeat(7) + "\n",
-                " ".repeat(3) + "x".repeat(0) + "|"
+                " ".repeat(level - 1) + "x".repeat(0) + "|"
             )
             printable.print(tree.joinToString(separator = "") { it })
         }
