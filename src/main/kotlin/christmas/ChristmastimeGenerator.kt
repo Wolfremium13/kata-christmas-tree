851d0d09b30a4a0319a5cafe9c3fc6605440d254
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-02-02 10:38:43 +0000

    ♻ extract space repetition logic

--- src/main/kotlin/christmas/ChristmastimeGenerator.kt
+++ src/main/kotlin/christmas/ChristmastimeGenerator.kt
@@ -6,29 +6,29 @@ class ChristmastimeGenerator(val printable: Printable) {
         giveOnlyTheTrunkWhenLevelIsZero(level)
         if (level == 2) {
             val tree = listOf(
-                " x\n",
-                "xxx\n",
-                " |"
+                " ".repeat(1) + "x\n",
+                " ".repeat(0) + "xxx\n",
+                " ".repeat(1) + "|"
             )
             printable.print(tree.joinToString(separator = "") { it })
         }
         if (level == 3) {
             val tree = listOf(
-                "  x\n",
-                " xxx\n",
-                "xxxxx\n",
-                "  |"
+                " ".repeat(2) + "x\n",
+                " ".repeat(1) + "xxx\n",
+                " ".repeat(0) + "xxxxx\n",
+                " ".repeat(2) + "|"
             )
             printable.print(tree.joinToString(separator = "") { it })
         }
 
         if (level == 4) {
             val tree = listOf(
-                "   x\n",
-                "  xxx\n",
-                " xxxxx\n",
-                "xxxxxxx\n",
-                "   |"
+                " ".repeat(3) + "x\n",
+                " ".repeat(2) + "xxx\n",
+                " ".repeat(1) + "xxxxx\n",
+                " ".repeat(0) + "xxxxxxx\n",
+                " ".repeat(3) + "|"
             )
             printable.print(tree.joinToString(separator = "") { it })
         }
